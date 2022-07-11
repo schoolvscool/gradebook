2f66ddd8f91c89ab7a64cd3b69790178da95b1ff
Nathaniel Low <schoolvscool@gmail.com>
2022-07-10 22:10:18 -0700

    Update English answer

--- src/main/kotlin/Answers.kt
+++ src/main/kotlin/Answers.kt
@@ -1,6 +1,6 @@
 val Answers = mapOf(
     Subject.ART to "1968",
-    Subject.ENGLISH to "the mortified times ceaselessly equal",
+    Subject.ENGLISH to "question not the current animals",
     Subject.HISTORY to "lighthouse",
     Subject.MATH to "101",
     Subject.SCIENCE to "temperature",
